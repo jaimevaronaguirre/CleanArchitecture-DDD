b3826c2b012f0f8ed0dea1a8df5592b84f21a5d6
Jaime Segundo Varon Aguirre <kenshingjv@hotmail.com>
2026-08-04 11:36:29 -0500

    Avances del curso: [actualización paginación de vehículos]

---
+++ src/CleanArchitecture/CleanArchitecture.Application/Vehiculos/GetVehiculosByPagination/GetVehiculosByPaginationQueryHandler.cs
@@ -0,0 +1,24 @@
+﻿using CleanArchitecture.Application.Abstractions.Messaging;
+using CleanArchitecture.Domain.Abstractions;
+using CleanArchitecture.Domain.Vehiculos;
+
+namespace CleanArchitecture.Application.Vehiculos.GetVehiculosByPagination
+{
+    internal sealed class GetVehiculosByPaginationQueryHandler
+    : IQueryHandler<GetVehiculosByPaginationQuery, PaginationResult<Vehiculo, VehiculoId>>
+    {
+        private readonly IVehiculoRepository _vehiculoRepository;
+
+        public GetVehiculosByPaginationQueryHandler(IVehiculoRepository vehiculoRepository)
+        {
+            _vehiculoRepository = vehiculoRepository;
+        }
+
+        public Task<Result<PaginationResult<Vehiculo, VehiculoId>>> Handle(
+            GetVehiculosByPaginationQuery request,
+            CancellationToken cancellationToken)
+        {
+            // hay que crear una instancia de una especificacion
+        }
+    }
+}
