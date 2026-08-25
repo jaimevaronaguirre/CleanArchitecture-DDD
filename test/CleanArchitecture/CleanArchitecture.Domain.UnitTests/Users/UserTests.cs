f39690b93e57a818c6971fcea157e6965fd1dc3f
Jaime Segundo Varon Aguirre <kenshingjv@hotmail.com>
2026-08-24 19:09:39 -0500

    Add domain unit tests for rentals and vehicles

--- test/CleanArchitecture/CleanArchitecture.Domain.UnitTests/Users/UserTests.cs
+++ test/CleanArchitecture/CleanArchitecture.Domain.UnitTests/Users/UserTests.cs
@@ -1,9 +1,13 @@
-﻿using CleanArchitecture.Domain.Users;
+﻿using CleanArchitecture.Domain.Roles;
+using CleanArchitecture.Domain.UnitTests.Infrastructure;
+using CleanArchitecture.Domain.Users;
+using CleanArchitecture.Domain.Users.Events;
+using FluentAssertions;
 using Xunit;
 
 namespace CleanArchitecture.Domain.UnitTests.Users
 {
-    public class UserTests
+    public class UserTests : BaseTest
     {
         [Fact]
         public void Create_Should_SetPropertyValues()
@@ -20,7 +24,40 @@ namespace CleanArchitecture.Domain.UnitTests.Users
             );
 
             // Assert
-
+            user.Nombre.Should().Be(UserMock.Nombre);
+            user.Apellido.Should().Be(UserMock.Apellido);
+            user.Email.Should().Be(UserMock.Email);
+            user.PasswordHash.Should().Be(UserMock.Password);     
         }
+
+        [Fact]
+        public void Create_Should_RaiseUserCreateDomainEvent()
+        {
+            var user = User.Create(
+                UserMock.Nombre,
+                UserMock.Apellido,
+                UserMock.Email,
+                UserMock.Password
+            );
+
+            // var domainEvent = user.GetDomainEvents().OfType<UserCreatedDomainEvent>().SingleOrDefault();
+
+            var domainEvent = AssertDomainEventWasPublished<UserCreatedDomainEvent>(user);
+
+            domainEvent!.UserId.Should().Be(user.Id);
+        }
+
+        [Fact]
+        public void Create_Should_AddRegisterRoleToUser()
+        {
+            var user = User.Create(
+                UserMock.Nombre,
+                UserMock.Apellido,
+                UserMock.Email,
+                UserMock.Password
+            );
+            user.Roles.Should().Contain(Role.Cliente);
+        }
+
     }
 }
---
+++ test/CleanArchitecture/CleanArchitecture.Domain.UnitTests/Vehiculos/VehiculoMock.cs
@@ -0,0 +1,23 @@
+﻿using CleanArchitecture.Domain.Shared;
+using CleanArchitecture.Domain.Vehiculos;
+
+namespace CleanArchitecture.Domain.UnitTests.Vehiculos
+{
+    internal static class VehiculoMock
+    {
+        public static Vehiculo Create(Moneda precio, Moneda? mantenimiento = null) 
+            => new(
+                VehiculoId.New(),
+                new Modelo("Corolla"),                
+                new Vin("45dsdfds544"),
+                precio,
+                mantenimiento ?? Moneda.Zero(),
+                DateTime.UtcNow.AddYears(-1),
+                [],
+                new Direccion("USA", "California", "Los Angeles", "El Paso", "Sunset Blvd")
+
+            );
+        
+       
+    }
+}
